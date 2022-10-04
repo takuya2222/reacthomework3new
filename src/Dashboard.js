9c28d351e4f5aadf9561cae5926f304d002aa621
takuya <hukujuth@gmail.com>
2022-10-04 22:04:57 +0900

    firestoreのデータを読み込み成功

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,28 +2,19 @@ import React, { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./FirebaseConfig.js";
 import db from "./FirebaseConfig";
-import { collection, getDoc, querySnapshot, doc } from "firebase/firestore";
+import { getDoc, doc } from "firebase/firestore";
 import { async } from "@firebase/util";
 
 const Dashboard = () => {
-  // async
-  // () => {
   const [userName, setUserName] = useState("");
-
-  //   const userData = collection(db, "users");
-  //   getDocs(userData).then((querySnapshot) => {
-  //     querySnapshot.docs.map((doc) => {
-  //       setUserName(doc.get("username"));
-  //     });
-  //     console.log(querySnapshot.docs);
-  //   });
+  const [balance, setBalance] = useState("");
 
   useEffect(() => {
     (async () => {
-      const docRef = doc(db, "users");
+      const docRef = doc(db, "users", "5UkEx4PkWs3uxfN8AVLZ");
       const docSnap = await getDoc(docRef);
-      setUserName(docSnap.username);
-      console.log(setUserName);
+      setUserName(docSnap.data().username);
+      setBalance(docSnap.data().balance);
     })();
   }, []);
 
@@ -38,7 +29,7 @@ const Dashboard = () => {
 
   return (
     <>
-      {userName}さんようこそ！
+      {userName}さんようこそ！ 残高:{balance}
       <h1>マイページ</h1>
       {/* ↓ユーザーのメールアドレスを表示（ログインしている場合） */}
       <p>{user && user.email}</p>
